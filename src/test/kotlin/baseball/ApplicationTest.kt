260285f1fdde446d1f86eb119d21aa1c373cb274
whk06061 <gju06061@naver.com>
2022-11-26 22:26:15 +0900

    test: 예외 발생 검사하는 테스트 제거
    try catch 문을 사용했기 때문에 예외 발생 검사하는 테스트 필요없음.

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -1,11 +1,9 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
-import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
@@ -20,12 +18,12 @@ class ApplicationTest : NsTest() {
         )
     }
 
-    @Test
-    fun `예외 테스트`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("1234") }
-        }
-    }
+//    @Test
+//    fun `예외 테스트`() {
+//        assertSimpleTest {
+//            assertThrows<IllegalArgumentException> { runException("1234") }
+//        }
+//    }
 
     override fun runMain() {
         main()
