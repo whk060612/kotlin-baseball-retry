31809e1bc96c85197448702de8dd97401682f592
whk06061 <gju06061@naver.com>
2022-11-26 22:41:06 +0900

    fix: 결과 출력 형식 버그 수정

--- src/main/kotlin/baseball/domain/Judgement.kt
+++ src/main/kotlin/baseball/domain/Judgement.kt
@@ -7,9 +7,9 @@ class Judgement {
     fun judge(player: List<Int>, computer: List<Int>): String {
         val result = compare.countSamePlace(player, computer)
         if ((result[0] == 0) and (result[1] == 0)) return Result.NOTHING.message
-        if (result[0] == 0) return "$result[1]${Result.STRIKE.message}"
-        if (result[1] == 0) return "$result[0]${Result.BALL.message}"
-        return "$result[0]${Result.BALL.message} $result[1]${Result.STRIKE.message}"
+        if (result[0] == 0) return "${result[1]}${Result.STRIKE.message}"
+        if (result[1] == 0) return "${result[0]}${Result.BALL.message}"
+        return "${result[0]}${Result.BALL.message} ${result[1]}${Result.STRIKE.message}"
     }
 
     enum class Result(val message: String) {
